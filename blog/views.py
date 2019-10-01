66cd3773e37fbfb66d701a709904b696af2b6d40
Jon-Martin Storm <jonmartin.storm@gmail.com>
2019-10-01 12:25:53 +0200

    Adding more function views and learning about templates

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "hello_world.html")
+    context = {
+        "title": "Hello there....",
+    }
+    return render(request, "hello_world.html", context)
+
+def about_page(request):
+    context = {
+        "title": "About us",
+    }
+    return render(request, "hello_world.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Contact us",
+    }
+    return render(request, "hello_world.html", context)
