f03e5abb753281ad58ff6d92131d897d33764525
Jon-Martin Storm <jonmartin.storm@gmail.com>
2019-10-10 08:37:56 +0200

    Added absolute url to blog model, and success_url for create blog post

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.utils import timezone
 from .utils import unique_slug_generator
+from django.urls import reverse
 
 
 User = settings.AUTH_USER_MODEL
@@ -37,6 +38,9 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post-detail', args=[self.slug])
 
     def save(self, *args, **kwargs):
         self.slug = unique_slug_generator(self, self.title)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponse
+# from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import FormView, CreateView, UpdateView
@@ -18,20 +18,23 @@ class BlogPostListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['now'] = timezone.now()
+        # context['now'] = timezone.now()
         return context
 
 class BlogCreateFormView(LoginRequiredMixin, FormView):
     template_name = "blog/create.html"
     form_class = BlogPostModelForm
-    success_url = '.'
+    object = None
 
     # 403 if user is not logged in.
     raise_exception = True
 
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
     def form_valid(self, form):
         form.instance.author = self.request.user
-        form.save()
+        self.object = form.save()
         return super().form_valid(form)
 
 class BlogPostDetailView(DetailView):
@@ -47,7 +50,7 @@ class BlogPostUpdateView(LoginRequiredMixin, UpdateView):
     model = BlogPost
     template_name = "blog/update.html"
     form_class = BlogPostModelForm
-    success_url = '/'
+    # success_url = '/'
 
     # 403 if user is not logged in.
     raise_exception = True
