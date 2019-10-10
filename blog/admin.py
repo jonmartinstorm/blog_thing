935e4581ab730fd0552cf4bbe7aee83258d2c13e
Jon-Martin Storm <jonmartin.storm@gmail.com>
2019-10-10 09:31:13 +0200

    Made list view better

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import BlogPost
 
 class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'id',)
+    list_display = ('title', 'slug', 'id', 'image')
 
 admin.site.register(BlogPost, BlogPostAdmin)
